7943f33be761956d6e117ea08c7ff6fbbb73f090
Victor <wanjala94vic@gmail.com>
2024-03-12 18:21:00 +0300

    updating msql

--- users/models.py
+++ users/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-
-
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
+    username = models.CharField(max_length=150, unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['username', 'name']
 
     def __str__(self):
         return self.email
+
 
 
 
